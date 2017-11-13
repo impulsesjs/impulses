c4559aa4beb2c80595228bf73546c433030a8cb5
João Correia <jca.correia@gmail.com>
2017-11-13 21:14:05 +0100

    Updated package and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
 const env = require('yargs').argv.env; // use --env with webpack 2
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 let libraryName = 'commbus';
 
@@ -15,6 +16,38 @@ if (env === 'build') {
 } else {
   outputFile = libraryName + '.js';
 }
+plugins.push(new BundleAnalyzerPlugin({
+    // Can be `server`, `static` or `disabled`.
+    // In `server` mode analyzer will start HTTP server to show bundle report.
+    // In `static` mode single HTML file with bundle report will be generated.
+    // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+    analyzerMode: 'static',
+    // Host that will be used in `server` mode to start HTTP server.
+    // analyzerHost: '127.0.0.1',
+    // Port that will be used in `server` mode to start HTTP server.
+    // analyzerPort: 8888,
+    // Path to bundle report file that will be generated in `static` mode.
+    // Relative to bundles output directory.
+    reportFilename: '../output-compile/index.html',
+    // Module sizes to show in report by default.
+    // Should be one of `stat`, `parsed` or `gzip`.
+    // See "Definitions" section for more information.
+    defaultSizes: 'parsed',
+    // Automatically open report in default browser
+    openAnalyzer: false,
+    // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+    generateStatsFile: false,
+    // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+    // Relative to bundles output directory.
+    statsFilename: '../output-compile/stats.json',
+    // Options for `stats.toJson()` method.
+    // For example you can exclude sources of your modules from stats file with `source: false` option.
+    // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+    statsOptions: null,
+    // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+    logLevel: 'info'
+  })
+)
 
 console.log('From Webpack:', __dirname)
 
